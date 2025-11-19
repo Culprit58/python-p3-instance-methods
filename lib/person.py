86b37b78ccbaebd27faf8b453acaf659d09e9e1f
Enock Cheruiyot <cheruiyote518@gmail.com>
2025-11-20 00:00:28 +0300

    lab done

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    # Instance method definitions
+    def walk(self):
+        print("The person is walking.")
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
